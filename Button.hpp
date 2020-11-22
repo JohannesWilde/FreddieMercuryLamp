32852f1283c2e53460fff93bfa814b490d27e89f
Johannes Wilde <git@jowil.de>
2020-11-22 15:05:11 +0100

    Purely visual refactoring of Button.hpp.

--- Button.hpp
+++ Button.hpp
@@ -7,56 +7,56 @@ template <int pinNumber>
 class Button
 {
 public:
-  Button()
-  {
-    pinMode(pinNumber, INPUT_PULLUP);
-    wasDownPreviously_ = isDown();
-  }
+    Button()
+    {
+        pinMode(pinNumber, INPUT_PULLUP);
+        wasDownPreviously_ = isDown();
+    }
 
-  ~Button()
-  {
-    pinMode(pinNumber, INPUT);
-  }
+    ~Button()
+    {
+        pinMode(pinNumber, INPUT);
+    }
 
-  /**
+    /**
    * isDown - button is currently being pressed down.
    */
-  bool isDown()
-  {
-	bool const isDownNow = isDown_();
-	wasDownPreviously_ = isDownNow;
-    return isDownNow;
-  }
+    bool isDown()
+    {
+        bool const isDownNow = isDown_();
+        wasDownPreviously_ = isDownNow;
+        return isDownNow;
+    }
 
-  /**
+    /**
    * pressed - button was pressed.
    */
-  bool pressed()
-  {
-	bool const isDownNow = isDown_();
-	bool const wasPressed = !wasDownPreviously_ && isDownNow;
-	wasDownPreviously_ = isDownNow;
-    return wasPressed;
-  }
+    bool pressed()
+    {
+        bool const isDownNow = isDown_();
+        bool const wasPressed = !wasDownPreviously_ && isDownNow;
+        wasDownPreviously_ = isDownNow;
+        return wasPressed;
+    }
 
-  /**
+    /**
    * released - button was released.
    */
-  bool released()
-  {
-	bool const isDownNow = isDown_();
-	bool const wasReleased = wasDownPreviously_ && !isDownNow;
-	wasDownPreviously_ = isDownNow;
-    return wasReleased;
-  }
+    bool released()
+    {
+        bool const isDownNow = isDown_();
+        bool const wasReleased = wasDownPreviously_ && !isDownNow;
+        wasDownPreviously_ = isDownNow;
+        return wasReleased;
+    }
 
 private:
-  bool wasDownPreviously_;
-  
-  bool isDown_()
-  {
-    return (LOW == digitalRead(pinNumber));
-  }  
+    bool wasDownPreviously_;
+
+    bool isDown_()
+    {
+        return (LOW == digitalRead(pinNumber));
+    }
 };
 
 #endif // BUTTON_HPP
