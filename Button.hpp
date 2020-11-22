0358404dde16d67f6d4060637a5a2e788633f58e
Johannes Wilde <git@jowil.de>
2020-11-22 15:26:23 +0100

    Add Button::getState method for access to all possible states with just one function call, as to avoid calling isDown and pressed right after each other and thus missing one pressed returning true in case isDown updated the wasDownPreviously_ member and thus overshadowed the press event.

--- Button.hpp
+++ Button.hpp
@@ -7,6 +7,21 @@ template <int pinNumber>
 class Button
 {
 public:
+
+    struct ButtonState
+    {
+        ButtonState(bool const isDown, bool const pressed, bool const released)
+            : isDown(isDown)
+            , pressed(pressed)
+            , released(released)
+        {
+        }
+
+        bool isDown : 1;
+        bool pressed : 1;
+        bool released : 1;
+    };
+
     Button()
     {
         pinMode(pinNumber, INPUT_PULLUP);
@@ -34,7 +49,7 @@ public:
     bool pressed()
     {
         bool const isDownNow = isDown_();
-        bool const wasPressed = !wasDownPreviously_ && isDownNow;
+        bool const wasPressed = pressed_(isDownNow, wasDownPreviously_);
         wasDownPreviously_ = isDownNow;
         return wasPressed;
     }
@@ -45,9 +60,23 @@ public:
     bool released()
     {
         bool const isDownNow = isDown_();
-        bool const wasReleased = wasDownPreviously_ && !isDownNow;
+        bool const wasReleased = released_(isDownNow, wasDownPreviously_);
         wasDownPreviously_ = isDownNow;
         return wasReleased;
+    }
+
+    /**
+     * @brief getState Return the current state of the button.
+     * @return ButtonState instance.
+     */
+    ButtonState getState()
+    {
+        bool const isDownNow = isDown_();
+        ButtonState const buttonState(isDownNow,
+                                      pressed_(isDownNow, wasDownPreviously_),
+                                      released_(isDownNow, wasDownPreviously_));
+        wasDownPreviously_ = isDownNow;
+        return buttonState;
     }
 
 private:
@@ -56,6 +85,16 @@ private:
     bool isDown_()
     {
         return (LOW == digitalRead(pinNumber));
+    }
+
+    static bool pressed_(bool const isDownNow, bool const wasDownPreviously)
+    {
+        return (!wasDownPreviously && isDownNow);
+    }
+
+    static bool released_(bool const isDownNow, bool const wasDownPreviously)
+    {
+        return (wasDownPreviously && !isDownNow);
     }
 };
 
